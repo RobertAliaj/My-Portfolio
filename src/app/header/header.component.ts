fbefe5552180aade5d8d82e7e4e24ee03639c35e
rOBI <robertaliaj97@gmail.com>
2023-05-01 14:25:19 +0200

    slide-out-menu styling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 
@@ -9,37 +9,12 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./header.component.scss']
 })
 
-export class HeaderComponent implements AfterViewInit {
+export class HeaderComponent {
 
   constructor(public translate: TranslateService) { }
 
-  @ViewChild('menuBtn') menuBtn!: ElementRef;
-
   german = false;
   menuOpen = false;
-
-
-
-  ngAfterViewInit(): void {
-    let menuBtn = this.menuBtn.nativeElement;
-
-    menuBtn.addEventListener('click', () => {
-      menuBtn.classList.toggle('open');
-      // !this.menuOpen ? this.openMenu(menuBtn) : this.closeMenu(menuBtn);
-    });
-  }
-
-
-  openMenu(menuBtn: HTMLElement) {
-    // menuBtn.classList.add('open');
-    this.menuOpen = true;
-  }
-
-  closeMenu(menuBtn: HTMLElement) {
-    // menuBtn.classList.remove('open');
-    this.menuOpen = false;
-  }
-
 
   germanImg() {
     this.german = false;
--- src/app/slide-out-menu/slide-out-menu.component.ts
+++ src/app/slide-out-menu/slide-out-menu.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-slide-out-menu',
   templateUrl: './slide-out-menu.component.html',
   styleUrls: ['./slide-out-menu.component.scss']
 })
-export class SlideOutMenuComponent {
+export class SlideOutMenuComponent implements AfterViewInit {
+  
+  @ViewChild('menuBtn') menuBtn!: ElementRef;
+  @ViewChild('menu') menu!: ElementRef;
+
+  toggleMenu() {
+    let menu = this.menu.nativeElement;
+    menu.classList.toggle('open');
+
+    if (menu.classList.contains('open')) {
+      var listItems = menu.querySelectorAll('ul li');
+      listItems.forEach(function (item: HTMLElement, index: number) {
+        item.style.animationDelay = (index * 100) + 'ms';
+      });
+    }
+  }
+
+
+
+
+
+
+  ngAfterViewInit(): void {
+    let menuBtn = this.menuBtn.nativeElement;
+    
+    menuBtn.addEventListener('click', () => {
+      menuBtn.classList.toggle('open');
+      this.toggleMenu();
+    });
+  }
 
 }
